aa9eba90d571880a5094be92b754e6fbf48b5d01
Daniel Trzaskoma <dtrzaskoma@gmail.com>
2021-04-01 18:36:11 +0200

    master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
+//zaimportowanie arkuszu stylów, są to style globalne
+//konflikt nazw
 import './App.css';
+import React from 'react';
+import Text from './Text';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//biblioteka bootstrapa
+//npm install bootstrap
+
+//preprocesor Sass
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isActive:false,
+      underline:false
+    }
+  }
+
+  handleClick=()=>{
+    this.setState({
+      isActive:!this.state.isActive,
+      underline:!this.state.underline
+    })
+  }
+
+  render() { 
+
+    //to jest stylowanie lokalne
+    const btn_off = {
+      border:'4px solid black', 
+      padding:'10px 20px',
+      fontFamily:'arial',
+      fontSize:30,
+      display:'block',
+      margin:'20px auto',
+      backgroundColor:'white',
+    }
+    const btn_on = {
+      border:'4px solid black', 
+      padding:'10px 20px',
+      fontFamily:'arial',
+      fontSize:30,
+      display:'block',
+      margin:'20px auto',
+      backgroundColor:'green',
+      color:'white'
+    }
+
+    let styles = this.state.isActive ?  btn_off : btn_on; 
+
+    const text = "Witaj na stronie!"
+
+    let classes="";
+    if(this.state.underline){
+      classes+=" line";
+    }
+
+    if(text.length > 12){
+      classes+=" big";
+    }
+
+    // style z tablicy - trzeba użyć joina
+    const classesTab = ['one'];
+
+    if(this.state.underline){
+      classesTab.push('line')
+    }
+
+    if(text.length > 20){
+      classesTab.push('big')
+    }
+
+    return (<div>
+      <h1>Stylowanie lokalne</h1>
+      <button 
+      onClick={this.handleClick} 
+      style={styles}>
+        {this.state.isActive?"Wyłącz":"Włącz"}
+        </button>
+
+        <h1>Stylowanie globalne</h1>
+        <div className="app">
+          <h1 className={classes}>Styl z arkusza</h1>
+          <h1 className={classes}>Zależny od długości tekstu</h1>
+          <h1 className={classesTab.join(" ")}>Z tablicy</h1>
+          <Text></Text>
+        </div>
+    </div>  );
+  }
 }
-
+ 
 export default App;
---
+++ src/Text.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Text.css"
+
+const Text = () => {
+    return (
+    <div>
+    <p className="red">
+        Oczywiście „około cztery tygodnie” to może być dmuchanie na zimne i okaże się, że już za 15-20 dni Lewandowski powróci na boisko, choćby w małym wymiarze czasowym. Jednak biorąc pod uwagę natężenie meczów w najbliższych tygodniach, wieści i tak są katastrofalne. „Lewego” prawdopodobnie zabraknie na murawie w obu hitowych starciach Ligi Mistrzów z Paris Saint-Germain. Przepadnie mu kilka meczów na szczycie także w Bundeslidze, a przewaga Bayernu nad resztą stawki nie jest wcale przesadnie wyśrubowana.
+    </p> 
+    <button className="btn btn-dark">Przycisk</button>
+    </div> );
+}
+ 
+export default Text;
